30c849bf0a10bb31bb09bc1cedb38f6de9069f26
Minh Hoang <hoangminhweb@gmail.com>
2016-12-05 02:36:56 +0700

    Update WifiWizard.js

--- www/WifiWizard.js
+++ www/WifiWizard.js
@@ -298,10 +298,12 @@ var WifiWizard = {
      * @param 	fail
      */
     isMobileDataEnabled: function(win, fail) {
+	alert(typeof win);
         if (typeof win != "function") {
             console.log("isMobileDataEnabled first parameter must be a function to handle Mobile Data status.");
             return;
         }
+ 	alert(win);
         cordova.exec(
             // Cordova can only return strings to JS, and the underlying plugin
             // sends a "1" for true and "0" for false.
