36b20d53aacbc16058b1d517881a722c7037eeb1
Minh Hoang <hoangminhweb@gmail.com>
2016-12-04 23:38:38 +0700

    Update WifiWizard.java

--- src/android/src/com/pylonproducts/wifiwizard/WifiWizard.java
+++ src/android/src/com/pylonproducts/wifiwizard/WifiWizard.java
@@ -584,7 +584,7 @@ public class WifiWizard extends CordovaPlugin {
         try {
             ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
             Method method = connectivityManager.getClass().getMethod("getMobileDataEnabled");
-			Object isEnabled = method.invoke(connectivityManager);
+			Boolean isEnabled = (Boolean) method.invoke(connectivityManager);
 			callbackContext.success(isEnabled ? "1" : "0");
             return (Boolean) isEnabled;
         } catch (Exception e) {
