660bcc54df15b7b4570581c11f706aa099e4982a
Minh Hoang <hoangminhweb@gmail.com>
2016-12-10 14:52:57 +0700

    Update WifiWizard.java

--- src/android/src/com/pylonproducts/wifiwizard/WifiWizard.java
+++ src/android/src/com/pylonproducts/wifiwizard/WifiWizard.java
@@ -657,12 +657,12 @@ public class WifiWizard extends CordovaPlugin {
       }	 
       
       if(currentapiVersion < 23){
-	 if(status === true)
+	 if(status)
           setMobileDataEnabled(true);
 	 else
 	  setMobileDataEnabled(false);
       }else{
-	 if(status === true)
+	 if(status)
            setMobileDataEnabledAndL(true);
 	 else
 	   setMobileDataEnabledAndL(false);	  
